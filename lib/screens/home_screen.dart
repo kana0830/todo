c5bccbbff567fc1abe39d08e30d3f68538573755
kana <kf0830s@gmail.com>
2023-11-21 20:00:46 +0900

    Riverpodパッケージの追加

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:todo/viewmodel/checkBoxListModel.dart';
+import 'package:todo/viewmodel/check_box_list_model.dart';
 
-import '../viewmodel/checkBoxListModel.dart';
-import '../viewmodel/checkBoxListModel.dart';
+import '../viewmodel/check_box_list_model.dart';
+import '../viewmodel/check_box_list_model.dart';
 
 class HomeScreen extends StatelessWidget {
   // void moveToCreatePage(context) {
@@ -51,48 +51,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({super.key});
-//
-//   @override
-//   State<HomeScreen> createState() => _HomeScreenState();
-// }
-//
-// class _HomeScreenState extends State<HomeScreen> {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     var todos = [
-//       Todo(title: '散歩', subTitle: '10,000歩以上', isCheck: false, ),
-//       Todo(title: '筋トレ', subTitle: '胸・腕の日', isCheck: false, ),
-//       Todo(title: '勉強', subTitle: 'flutterのDB取得', isCheck: false, ),
-//       Todo(title: '買い物', subTitle: '旅行グッズ買う', isCheck: false, ),
-//       Todo(title: '掃除', subTitle: 'お風呂・キッチン', isCheck: false, ),
-//       Todo(title: '美容院', subTitle: 'アイロンとシャンプーも買う', isCheck: false, ),
-//     ];
-//
-//
-//     final list = ListView.builder(
-//       itemCount: todos.length,
-//       itemBuilder: (c, i) => CheckBoxListModel().modelToWidget(todos[i]),
-//     );
-//
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("TODO一覧"),
-//       ),
-//       body: FutureBuilder<QuerySnapshot>(
-//         future: FirebaseFirestore.instance.collection('todo').get(),
-//         builder: ((context, snapshot) {
-//           if (!snapshot.hasData){
-//             return const Center(child: CircularProgressIndicator(),
-//             );
-//           }
-//
-//
-//         }).toList();
-//       )
-//     );
-//   }
-// }
--- lib/viewmodel/check_box_list_model.dart
+++ lib/viewmodel/check_box_list_model.dart
@@ -1,13 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:todo/screens/home_screen.dart';
-
-import '../repository/todo.dart';
 
 class CheckBoxListModel {
   Future<String> get() async {
     CollectionReference todo = FirebaseFirestore.instance.collection('todo');
-    final doc = await todo.doc('1').get();
     return todo.toString();
   }
 
@@ -16,14 +12,18 @@ class CheckBoxListModel {
     // タイトル
     final task = document['task'];
     final detail = document['detail'];
-    final endFlg = document['endFlg'];
+    var endFlg = document['endFlg'];
+
+    void itemChange(bool value) {
+      endFlg = value;
+    }
 
     // 部品を並べる
     return Card(
       child: CheckboxListTile(
         value: endFlg,
         onChanged: (value) {
-          // itemChange(document, value!);
+          itemChange(value!);
         },
         title: Text(task),
         subtitle: Text(detail),
@@ -32,7 +32,5 @@ class CheckBoxListModel {
     );
   }
 
-// void itemChange(var model, bool value) {
-//   model.isCheck = value;
-// }
+
 }
