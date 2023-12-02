24074518718f23394b52018fd6cc24edfb516e7b
kana <kf0830s@gmail.com>
2023-12-02 10:41:11 +0900

    変更途中

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:todo/repository/todo.dart';
 import 'package:todo/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
