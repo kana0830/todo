c26ef70bd75fd861678652cb3807c74b38f23e42
kana <kf0830s@gmail.com>
2023-12-22 20:31:30 +0900

    変更途中

--- lib/viewmodel/todo_notifier.dart
+++ lib/viewmodel/todo_notifier.dart
@@ -1,19 +1,25 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../repository/todo.dart';
-
-class TodosNotifier extends StateNotifier<CollectionReference<Map<String, dynamic>>> {
-  TodosNotifier() : super(FirebaseFirestore.instance.collection('todo'));
-
-  void get() {
-    state = FirebaseFirestore.instance.collection('todo');
-  }
-  void addTodo(Todo todo) {}
-
-  void removeTodo(String todoId) {}
-
-}
-
-final todosProvider = StateNotifierProvider<TodosNotifier, CollectionReference<Map<String, dynamic>>>((ref) {
-  return TodosNotifier();
-});
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import '../repository/todo.dart';
+//
+// class TodosNotifier extends StateNotifier<List<Todo>> {
+//   TodosNotifier(): super([]);
+//   var todo = Todo().getTodoList();
+//
+//   Future<List<String>> get() async {
+//     return todo;
+//   }
+//   void addTodo(Todo todo) {
+//
+//   }
+//
+//   void removeTodo(String todoId) {
+//
+//   }
+//
+// }
+//
+// final todosProvider = StateNotifierProvider<TodosNotifier, List<Todo>>((ref) {
+//   return TodosNotifier();
+// });
+//
