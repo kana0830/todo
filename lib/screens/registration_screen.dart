7623bb4a6bb416c4b5830d86fe9df232a216c1b5
kana <kf0830s@gmail.com>
2023-12-29 18:36:13 +0900

    登録画面の作成

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -5,6 +5,6 @@ class RegistrationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(child: Scaffold());
   }
 }
